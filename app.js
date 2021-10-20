4dc5c3fc387a29f56cc6e5971b930c28081730e8
Pawtang <ben.dibuz@gmail.com>
2021-10-20 16:15:58 -0500

    center

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
+const follower = document.querySelector('.follower')
 let x, y
+let size = 100
 
+function reducesize() {
+    size--;
+    follower.style.width = `${size}px`;
+    follower.style.height = `${size}px`;
+}
 
 document.addEventListener('mousemove', e=> {
-    let w = window.screen.availWidth;
-    let h = window.screen.availHeight;
+    let w = window.innerWidth;
+    let h = window.innerHeight;
     x = e.clientX/w;
     y = (e.clientY/h);
     rgbx = x*255
     rgby = y*255
-    document.getElementById('printer').innerText = `${x}, ${y}`
+    document.getElementById('printer').innerText = `${x.toFixed(2)}, ${y.toFixed(2)}`
     document.body.style.backgroundColor = `rgb(${rgbx}, ${rgby}, ${50})`;
-    document.querySelector('.follower').style.left = `${x*100}vw`
-    document.querySelector('.follower').style.top = `${y*100}vh`
+    follower.style.backgroundColor = `rgb(${rgby}, ${rgbx}, ${rgbx+rgby})`;
+    follower.style.left = `${x*100}vw`
+    follower.style.top = `${y*100}vh`
 })
 
+document.addEventListener('keypress', reducesize);
